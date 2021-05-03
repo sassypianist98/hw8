f8e5440953d551b715e5cbbaf78fe70829f7b977
Sarah Shamsie <sshamsie@Sarahs-MacBook-Pro.local>
2021-05-03 14:31:38 -0400

    added comments to IListings and IEngine

--- src/hw8/IEngine.java
+++ src/hw8/IEngine.java
@@ -1,17 +1,20 @@
 package hw8;
 
 import java.util.Collection;
-import java.util.Map;
 
+/**
+ * Interface for the IEngine data structure
+ * 
+ * @author Tiffany Chen, Sarah Engheta, Sarah Shamsie
+ *
+ */
 public interface IEngine {
-    
-    
+
     /**
      * max number of listings displayed (e.g. show top ten listings)
      */
     public static final int MAX_NUM_DISPLAY = 10;
 
-    
     /**
      * Reads in data from file
      * 
@@ -19,76 +22,64 @@ public interface IEngine {
      * @return listings
      */
     public Collection<IListing> getListings(String fileName);
-    
-    
+
     /**
      * Prints listing information
      * 
      * @param listings
      */
     public void printListings(Collection<IListing> listings);
-    
-
 
     /**
-     * checks whether listing's price falls in the range of user preference
-     *if it does fall in the range, set priceCheck of listing = 1 
-     *otherwise, set priceCheck of listing = 0
+     * checks whether listing's price falls in the range of user preference if
+     * it does fall in the range, set priceCheck of listing = 1 otherwise, set
+     * priceCheck of listing = 0
      * 
-     * @param listing, upperbound of price preference, lowerbound of price preference
+     * @param listing, upperbound of price preference, lowerbound of price
+     *                 preference
      */
     public void checkPrice(IListing listing, int upperBound, int lowerBound);
-    
-    
+
     /**
-     * checks whether listing's distance from epicenter falls in range of user preference
-     * if yes, set distanceCheck of listing =1
-     * if no, set distanceCheck of listing =0
+     * checks whether listing's distance from epicenter falls in range of user
+     * preference if yes, set distanceCheck of listing =1 if no, set
+     * distanceCheck of listing =0
      * 
      * @param listing, distance max
      */
-    public void checkDistance (IListing listing, int maxDistance);
-
+    public void checkDistance(IListing listing, int maxDistance);
 
     /**
      * checks whether listing's numOfReviews falls in range of user preference
-     * if yes, set reviewsCheck = 1
-     * if no, set reviewsCheck = 0
+     * if yes, set reviewsCheck = 1 if no, set reviewsCheck = 0
      * 
      * @param listing, numReviews minimum
      */
-    public void checkReviews (IListing listing, int numReviewsMin);
+    public void checkReviews(IListing listing, int numReviewsMin);
 
-    
     /**
-     * checks whether listing's property type is aligned with user preference
-     * if yes, set propertyTypeCheck =1
-     * if no, set propertyTypeCheck =0
+     * checks whether listing's property type is aligned with user preference if
+     * yes, set propertyTypeCheck =1 if no, set propertyTypeCheck =0
      * 
      * @param listing, property type
      */
     public void checkPropertyType(IListing listing, String propertyType);
-    
-    
+
     /**
-     * checks whether listing's room type is aligned with user preference
-     * if yes, set roomTypeCheck =1
-     * if no, set roomTypeCheck =0
+     * checks whether listing's room type is aligned with user preference if
+     * yes, set roomTypeCheck =1 if no, set roomTypeCheck =0
      * 
      * @param listing, roomType
      */
-    public void checkRoomType (IListing listing, String roomType);
-    
+    public void checkRoomType(IListing listing, String roomType);
 
     /**
      * checks whether listing's accommodates num is aligned with user preference
-     * if yes, set accommodatesCheck =1
-     * if no, set accommodatesCheck =0
+     * if yes, set accommodatesCheck =1 if no, set accommodatesCheck =0
      * 
      * @param listing, accommodates num
      */
     public void checkAccommodates(IListing listing, int accommodates);
-    
 
     /*
      * outputs list of property types
@@ -96,14 +87,12 @@ public interface IEngine {
      * @param listings
      */
     public Collection<String> getPropertyType(Collection<IListing> listings);
-    
-    
+
     /*
      * outputs list of room types
      * 
      * @param listings
      */
-    public Collection<String> getRoomType (Collection<IListing> listings);
-    
+    public Collection<String> getRoomType(Collection<IListing> listings);
 
 }
--- src/hw8/IListing.java
+++ src/hw8/IListing.java
@@ -5,55 +5,73 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Interface for the IListings data structures
+ * 
+ * @author Tiffany Chen, Sarah Engheta, Sarah Shamsie
+ *
+ */
 public interface IListing extends Comparable<IListing> {
-    /**
-     * Distances between zip codes and epicenter
-     */
-    public static final Map<Integer, Double> DISTANCES = null;
-    
-    /**
-     * Maps rank to percentage
-     */
-    public static final HashMap<Integer, Double> RANK = new HashMap<Integer, Double>() {{
-        put(1, 0.04);
-        put(2, 0.09);
-        put(3, 0.14);
-        put(4, 0.19);
-        put(5, 0.24);
-        put(6, 0.3);
-    }};
 
     /**
-     * Sorts the listings in descending order by percentage
+     * This map stores the distances between the listings' zip codes and an
+     * epicenter in SF
      * 
-     * @return comparator Object
+     * @TODO: pull these numbers from Google Maps
+     */
+    public static final Map<Integer, Double> DISTANCES = null;
+
+    /**
+     * This map stores the percentage breakdown applied to each rank (1-6, where
+     * 1 is the highest priority)
+     */
+    public static final HashMap<Integer, Double> RANK = new HashMap<Integer, Double>() {
+        {
+            put(1, 0.04);
+            put(2, 0.09);
+            put(3, 0.14);
+            put(4, 0.19);
+            put(5, 0.24);
+            put(6, 0.3);
+        }
+    };
+
+    /**
+     * Sorts the listings in descending order by final score (a percentage from
+     * 0-100)
+     * 
+     * @return comparator object for the listings
      */
     public static Comparator<IListing> byDescendingOrder() {
         return null;
     }
 
     /**
-     * Takes user params and returns k closest listings that align
-     * with the users preferences
+     * Takes user params and returns k closest listings that align with the
+     * users preferences for continuous variables only
      * 
      * @param k, lowerBoundPrice, upperBoundPrice, distance, numReviews
      * @return closest examples to query
+     * @TODO: clarify how to use this along with discrete variables
      */
-    public Collection<IListing> kNearestNeighbors(int k, double lowerBoundPrice, double
-            upperBoundPrice, double distance, int numReviews);
-    
+    public Collection<IListing> kNearestNeighbors(int k, double lowerBoundPrice,
+            double upperBoundPrice, double distance, int numReviews);
+
     /**
-     * Maps importance of user prefernce to rank
+     * As ther user ranks each feature type (1-6), it will be stored in a map
+     * associating each feature to a rank
      * 
-     * @param scores
-     * @return map of user params to ranking of importance
+     * @param user input scores (1-6)
+     * @return map of features to ranking of importance
      */
     public Map<String, Double> userRank(int[] scores);
-    
+
     /**
-     * Applies weights to each relevant parts of the listing
+     * This method calculates the "final score" associated with a listing based
+     * on the weighted percentage computed from its features, applying weights
+     * to each relevant part of the listing
      * 
-     * @param map
+     * @param map of feature type to user rank (1-6)
      * @return double score
      */
     public double computeScore(Map<String, Double> userRank);
